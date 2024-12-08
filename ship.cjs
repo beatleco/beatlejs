9bc830f38696465ec47050a1388e325ead285d7c
Aryan <alikhaniaryan@gmail.com>
2024-12-08 19:14:49 +0330

    remove peer dependencies

--- ship.cjs
+++ ship.cjs
@@ -2,20 +2,25 @@ const fs = require('fs/promises');
 const path = require('path');
 
 async function bootstrap() {
-  const data = await fs.readFile(
+  const packageJsonContents = await fs.readFile(
     path.resolve(__dirname, './package.json'),
     'utf8',
   );
-  const packageJson = JSON.parse(data);
-
-  delete packageJson.scripts;
-  delete packageJson.devDependencies;
-  packageJson.main = 'index.js';
-  packageJson.types = 'index.d.ts';
+  const packageJsonParsed = JSON.parse(packageJsonContents);
+  delete packageJsonParsed.scripts;
+  delete packageJsonParsed.devDependencies;
+  delete packageJsonParsed.peerDependencies;
+  packageJsonParsed.main = 'index.js';
+  packageJsonParsed.types = 'index.d.ts';
 
   fs.writeFile(
     path.resolve(__dirname, './build/package.json'),
-    JSON.stringify(packageJson, null, '  '),
+    JSON.stringify(packageJsonParsed, null, '  '),
+  );
+
+  await fs.copyFile(
+    path.resolve(__dirname, './README.md'),
+    path.resolve(__dirname, './build/README.md'),
   );
 }
 
